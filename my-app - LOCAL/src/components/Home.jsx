928ef71bba6839653f7c5c6dcceb21f9a39b1aae
MagaDev <magahr123@gmail.com>
2024-12-17 06:08:54 -0400

    17-12-2024 Updating the new project

--- my-app - LOCAL/src/components/Home.jsx
+++ my-app - LOCAL/src/components/Home.jsx
@@ -4,7 +4,7 @@ import { User } from './User';
 const Home = () => {
     return (
         <div className="container mt-2 text-center">
-            <h3>Actualización y Consulta de Usuarios</h3>
+            <h3>Actualización y Consulta de Usuarios desde mi LOCAL pero conectado a la BD AWS</h3>
             <User/>
         </div>
     )
